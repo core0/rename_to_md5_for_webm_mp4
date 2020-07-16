50eddb21f621284699c11860156bb9bf0478b2ea
MMihaylovskiy <core0@users.noreply.github.com>
2020-07-16 19:33:35 +0300

    Add files via upload

---
+++ rename_to_md5_for_webm_mp4.py
@@ -0,0 +1,25 @@
+﻿import os, sys
+import hashlib
+import datetime
+
+#ext = [".jpg",".png",".JPG",".JPEG",".PNG",".GIF",".gif"]
+ext = [".webm",".WEBM",".mp4",".MP4"]
+
+currentDT = datetime.datetime.now()
+print (str(currentDT))
+
+
+for p,fl,fls in os.walk(os.curdir):
+    for i in fls:
+        for j in ext:
+            if i.endswith(j):
+                nw_f =  hashlib.md5(open(i,"rb").read()).hexdigest()+j
+                try:
+                    os.rename(i,nw_f)
+                except WindowsError:
+                    os.remove(i)
+                break
+    break
+
+currentDT = datetime.datetime.now()
+print (str(currentDT))
